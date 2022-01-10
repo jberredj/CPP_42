7b573df66d78bb48f7129ab1a999f3c35dc360d9
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-10 11:36:24 +0100

    Add ex01

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,11 +6,12 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 15:18:11 by jberredj          #+#    #+#             */
-/*   Updated: 2022/01/10 11:33:50 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/01/10 11:33:38 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cmath>
 #include "Fixed.hpp"
 
 Fixed::Fixed(void)
@@ -18,6 +19,20 @@ Fixed::Fixed(void)
 {
 	if (Fixed::_verbose_print)
 		std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(int init_val)
+: _fixed(init_val << Fixed::_nbr_frac_bits)
+{
+	if (Fixed::_verbose_print)
+		std::cout << "Int constructor called" << std::endl;
+}
+
+Fixed::Fixed(float init_val)
+: _fixed(roundf(init_val * (1 << this->_nbr_frac_bits)))
+{
+	if (Fixed::_verbose_print)
+		std::cout << "Float constructor called" << std::endl;
 }
 
 Fixed::Fixed(const Fixed &obj)
@@ -33,7 +48,7 @@ Fixed::~Fixed()
 		std::cout << "Destructor called" << std::endl;
 }
 
-Fixed				&Fixed::operator=(const Fixed &to_asign)
+Fixed	&Fixed::operator=(const Fixed &to_asign)
 {
 	if (Fixed::_verbose_print)
 		std::cout << "Assignation operator called" << std::endl;
@@ -41,21 +56,31 @@ Fixed				&Fixed::operator=(const Fixed &to_asign)
 	return (*this);
 }
 
-int					Fixed::getRawBits(void) const
+int	Fixed::getRawBits(void) const
 {
 	if (Fixed::_verbose_print)
 		std::cout << "getRawBits member function called" << std::endl;
 	return (this->_fixed);
 }
 
-void				Fixed::setRawBits(int const raw)
+void	Fixed::setRawBits(int const raw)
 {
 	if (Fixed::_verbose_print)
 		std::cout << "setRawBits member function called" << std::endl;
 	this->_fixed = raw;
 }
 
-void			Fixed::set_verbose(bool new_val)
+int	Fixed::toInt(void) const
+{
+	return (this->_fixed >> this->_nbr_frac_bits);
+}
+
+float	Fixed::toFloat(void) const
+{
+	return ((float)this->_fixed / (float)(1 << this->_nbr_frac_bits));
+}
+
+void	Fixed::set_verbose(bool new_val)
 {
 	Fixed::_verbose_print = new_val;
 }
@@ -63,3 +88,9 @@ void			Fixed::set_verbose(bool new_val)
 
 const int	Fixed::_nbr_frac_bits = 8;
 bool	Fixed::_verbose_print = true;
+
+std::ostream	&operator<<(std::ostream &out, const Fixed &fixed)
+{
+	out << fixed.toFloat();
+	return (out);
+}
