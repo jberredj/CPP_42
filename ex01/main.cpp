bd210ea0266d182c4b7a5941840b0f416ebbb9ff
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-10 20:36:19 +0100

    Add ex01

---
+++ ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 19:44:38 by jberredj          #+#    #+#             */
+/*   Updated: 2022/01/10 20:35:48 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+int	main(void)
+{
+	ScavTrap	default_clap;
+	ScavTrap	linus("Linus");
+
+	default_clap.set_attack_damage(3);
+	default_clap.attack(linus.get_name());
+	linus.takeDamage(default_clap.get_attack_damage());
+	linus.beRepaired(10);
+	linus.set_attack_damage(42069);
+	linus.attack(default_clap.get_name());
+	default_clap.takeDamage(linus.get_attack_damage());
+	linus.set_attack_damage(12);
+	linus.attack(default_clap.get_name());
+	default_clap.takeDamage(linus.get_attack_damage());
+	default_clap.beRepaired(2);
+	linus.set_attack_damage(10);
+	linus.attack(default_clap.get_name());
+	default_clap.takeDamage(10);
+	linus.guardGate();
+	return (0);
+}
