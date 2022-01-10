e64fa0205a45b6967624f7f08d5c3172b39483f8
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-10 17:39:18 +0100

    Add ex03

---
+++ ex03/Point.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/10 12:20:54 by jberredj          #+#    #+#             */
+/*   Updated: 2022/01/10 17:39:12 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Point.hpp"
+
+Point::Point(void)
+: x(0), y(0)
+{
+}
+
+Point::Point(float new_x, float new_y)
+: x(new_x), y(new_y)
+{
+}
+
+
+Point::Point(const Point &obj)
+: x(obj.x), y(obj.y)
+{
+	*this = obj;
+}
+
+Point::~Point()
+{
+}
+
+// Useless operator=, x and y are const, and therefor can't be changed.
+Point	&Point::operator=(const Point &to_asign)
+{
+	(void)to_asign;
+	return (*this);
+}
+
+const Fixed Point::get_x(void) const
+{
+	return (this->x);
+}
+
+const Fixed Point::get_y(void) const
+{
+	return (this->y);
+}
