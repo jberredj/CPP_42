107cc126513037f83a1425e6c7b19da2eba87cb2
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-12 09:33:44 +0100

    Add log Form

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 20:16:52 by jberredj          #+#    #+#             */
-/*   Updated: 2022/01/11 23:45:49 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/01/12 09:33:01 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,12 @@ int	main(void)
 		std::cout << "YOU WILL BE FIRED" << std::endl;
 	}
 	form = slave.makeForm("shrubbery request", "Test");
+	std::cout << *form <<  std::endl;
 	form->beSigned(lvl_50_boss);
 	lvl_50_boss.executeForm(*form);
 	delete (form);
 	form = slave.makeForm("robotomy request", "Crook");
+	std::cout << *form <<  std::endl;
 	form->beSigned(lvl_50_boss);
 	lvl_50_boss.executeForm(*form);
 	delete (form);
