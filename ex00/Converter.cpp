cbb541d4832bd7b948181a6ec0de21fa1915a103
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-12 15:37:01 +0100

    Add ex00

---
+++ ex00/Converter.cpp
@@ -0,0 +1,209 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Converter.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 11:19:15 by jberredj          #+#    #+#             */
+/*   Updated: 2022/01/12 15:34:36 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <iomanip>
+#include <iostream>
+#include "Converter.hpp"
+
+const std::string	Converter::_special_val[8] =
+{
+	"inff",
+	"+inff",
+	"-inff",
+	"nanf",
+	"inf",
+	"+inf",
+	"-inf",
+	"nan"
+};
+
+// A little explanation on type selection.
+// (i / 4) + 2, because i is an int, when divided by 4, it land on 0 until i = 4
+// and so on 1 from 4 to 7. Here I add 2 to select either type Float or Double
+bool	Converter::_check_special_val(std::string &input)
+{
+	int i = 0;
+
+	while (i < 8 && input != Converter::_special_val[i])
+		i++;
+	if (i == 8)
+		return false;
+	this->_type = (i / 4) + 2;
+	this->_nan = true;
+	return (true);
+}
+
+void	Converter::_valid_input(std::string &input)
+{
+	size_t	len;
+	size_t	i;
+	int		dot;
+	int		f;
+
+	if (this->_check_special_val(input))
+		return ;
+	len = input.length();
+	if (len == 1 && (input[0] < '0' || input[0] > '9'))
+	{
+		this->_type = CHAR;
+		return ;
+	}
+	dot = 0;
+	f = 0;
+	i = (input[0] == '-' || input[0] == '+');
+	while (i < len && dot < 2 && f < 2)
+	{
+		if ((input[i] < 0x20 && input[i] >= 0x7F)
+			|| ((input[i] < '0' || input[i] > '9')
+			&& input[i] != '.' && input[i] != 'f'))
+			throw (Converter::StringInvalid());
+		if (input[i] == '.')
+			dot++;
+		if (input[i] == 'f')
+			f++;
+		i++;
+	}
+	if (f > 1 || (f == 1 && input[len - 1] != 'f')
+		|| dot > 1)
+		throw (Converter::StringInvalid());
+	this->_type = 1 + (2 * dot) - f;
+}
+
+size_t	Converter::_get_min_prec(void)
+{
+	float	base;
+	int		conv;
+	int		i = 1;
+
+	base = this->_d_conv - this->_i_conv;
+	while (base != (int)base)
+		base *= 10;
+	conv = base;
+	while (conv >= 10)
+	{
+		conv /= 10;
+		i++;
+	}
+	return (i);
+}
+
+Converter::Converter(void)
+: _type(NO_TYPE), _nan(false), _c_conv(0),
+	_i_conv(0), _f_conv(0), _d_conv(0)
+{
+}
+
+Converter::Converter(const Converter &src)
+: _type(src._type), _nan(src._nan), _c_conv(src._c_conv),
+	_i_conv(src._i_conv), _f_conv(src._f_conv), _d_conv(src._d_conv)
+{
+}
+
+Converter::~Converter(void)
+{
+}
+
+Converter	&Converter::operator=(const Converter &rhs)
+{
+	this->_type = rhs._type;
+	this->_nan = rhs._nan;
+	this->_c_conv = rhs._c_conv;
+	this->_i_conv = rhs._i_conv;
+	this->_f_conv = rhs._f_conv;
+	this->_d_conv = rhs._d_conv;
+	return (*this);
+}
+
+void	Converter::convert_str(std::string input)
+{
+	this->reset();
+	this->_valid_input(input);
+	switch (this->_type)
+	{
+	case CHAR:
+		this->_c_conv = input[0];
+		this->_i_conv = (int)this->_c_conv;
+		this->_d_conv = (double)this->_i_conv;
+		this->_f_conv = (double)this->_i_conv;
+		break;
+	case INT:
+		this->_i_conv = atoi(input.c_str());
+		this->_c_conv = (char)this->_i_conv;
+		this->_d_conv = (double)this->_i_conv;
+		this->_f_conv = (double)this->_i_conv;
+		break;
+	case FLOAT:
+		__attribute__ ((fallthrough));
+	case DOUBLE:
+		this->_f_conv = atof(input.c_str());
+		this->_d_conv = (double)atof(input.c_str());
+		this->_i_conv = (int)this->_d_conv;
+		this->_c_conv = (char)this->_i_conv;
+		break;
+	default:
+		throw (Converter::StringInvalid());
+		break;
+	}
+}
+
+char	Converter::get_c(void)
+{
+	return(this->_c_conv);
+}
+
+int	Converter::get_i(void)
+{
+	return(this->_i_conv);
+}
+
+float	Converter::get_f(void)
+{
+	return(this->_f_conv);
+}
+
+double	Converter::get_d(void)
+{
+	return(this->_d_conv);
+}
+
+void	Converter::print_all(void)
+{
+	std::cout << "char: ";
+	if (this->_nan)
+		std::cout << "Impossible" << std::endl;
+	else if (this->_c_conv < 0x20 || this->_c_conv >= 0x7F)
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << this->_c_conv << std::endl;
+	std::cout << "int: ";
+	if (this->_nan)
+		std::cout << "Impossible" << std::endl;
+	else
+		std::cout << this->_i_conv << std::endl;
+	std::cout << "float: ";
+	if (!this->_nan)
+		std::cout << std::setprecision(this->_get_min_prec());
+	std::cout << std::fixed;
+	std::cout << this->_f_conv << 'f' << std::endl;
+	std::cout << "double: " << this->_d_conv << std::endl;
+}
+
+void	Converter::reset(void)
+{
+	this->_type = NO_TYPE;
+	this->_nan = false;
+	this->_c_conv = 0;
+	this->_i_conv = 0;
+	this->_f_conv = 0;
+	this->_d_conv = 0;
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 09:48:07 by jberredj          #+#    #+#             */
+/*   Updated: 2022/01/12 15:36:05 by jberredj         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include "Converter.hpp"
+
+int	main (int ac, char **av)
+{
+	int			i = 1;
+	Converter	conv;
+	
+	while (i < ac)
+	{
+		try
+		{
+			if (av[i][0] == '\0')
+				throw(Converter::StringInvalid());
+			conv.convert_str(av[i]);
+			conv.print_all();
+			std::cout << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		i++;
+	}
+	return (0);
+}
