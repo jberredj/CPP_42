2a215cdd87eb38a3069f0c63881128a8d0f5f219
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-01-21 16:08:00 +0100

    Correct after evaluation

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 14:44:12 by jberredj          #+#    #+#             */
-/*   Updated: 2022/01/06 14:48:31 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/01/21 16:06:46 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	main(void)
 	std::string	*stringPTR = &brain;
 	std::string	&stringREF = brain;
 
+	std::cout << "stringPTR addr: " << stringPTR << std::endl;
+	std::cout << "stringREF addr: " << &stringREF << std::endl;
 	std::cout << "    brain says: " << brain << std::endl;
 	std::cout << "stringPTR says: " << *stringPTR << std::endl;
 	std::cout << "stringREF says: " << stringREF << std::endl;
