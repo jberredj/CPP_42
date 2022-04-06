d0b22c206102bd82d2fcfc8add1d2c32e4091d55
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-04-06 16:14:35 +0200

    Add a test

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jberredj <jberredj@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/12 16:40:30 by jberredj          #+#    #+#             */
-/*   Updated: 2022/01/12 17:05:05 by jberredj         ###   ########.fr       */
+/*   Updated: 2022/04/06 16:10:43 by jberredj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,25 @@ void	print(std::string &str)
 	std::cout << str << std::endl;
 }
 
+template<typename T>
+void	print_tpl(T &elem)
+{
+	std::cout << elem << std::endl;
+}
+
 int	main(void)
 {
 	std::string	array[15];
 	
 	::iter(array, 15, &init);
 	::iter(array, 15, &print);
+	std::cout << std::endl;
 	::iter(array, 15, &append_rand);
 	::iter(array, 15, &print);
+	std::cout << std::endl;
+	::iter(array, 15, &print_tpl);
+	int			new_arr[20];
+	for (int i = 0; i < 20; i++)
+		new_arr[i] = i;
+	::iter(new_arr, 20, &print_tpl);
 }
